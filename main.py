7d429923c44b2b9e954b7ff1444e4ad3ea1ea194
Siddharth_mishra <heaven.mishra@gmail.com>
2023-07-11 08:09:06 +0530

    added filterout function

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from utility import *
 
-with open("d:\\CI_CDdemo\\event_deployment\\test_1\\test1.ipynb","r") as file:
+with open("d:\\CI_CDdemo\\event_deployment\\test_1\\test2.ipynb","r") as file:
     raw_content = file.read()
     file.close()
 
 content = convert_from_json(raw_content)
 final_content = filterout(content)
-print(final_content)
 
-# print(generate_path())
+for i in final_content:
+    print(i)
--- utility.py
+++ utility.py
@@ -11,12 +11,42 @@ def convert_from_json(raw_content):
     return content.strip()
 
 def filterout(content):
-    final_content=[]
-    content_lst=content.split("\n\n")
-    for i in content_lst:
-        if "spark.sql(\"\"\"" in i and "\"\"\"" in i:
-            final_content.append(i)
-    return final_content
+    with open("raw_content.txt" , "w") as files:
+        files.write(content)
+
+    with open("raw_content.txt","r") as files:
+        final_content = files.readlines()
+
+    if os.path.exists("raw_content.txt"):
+        os.remove("raw_content.txt")
+    else:
+        print("file does not exist:")
+    
+    content_starting_point=0
+    for i in range(len(final_content)):
+        if "spark.sql(\"\"\"" in final_content[i] :
+            content_starting_point=i
+            break
+    block_starting_point=[]
+    for i in final_content[content_starting_point:]:
+        if "spark.sql(\"\"\"" in i:
+            block_starting_point.append(final_content.index(i))
+
+    block_ending_point = block_starting_point[1:].copy()
+    block_ending_point.append(len(final_content))
+    # print(block_starting_point)
+    # print(block_ending_point)
+    blocks=[]
+    for i in range(len(block_starting_point)):
+        blocks.append(final_content[block_starting_point[i]:block_ending_point[i]])
+
+    string_blocks=[]
+    for i in blocks:
+        strings=''
+        for j in i:
+            strings= strings + j
+        string_blocks.append(strings)
+    return string_blocks
 
 def extract_query(final_content):
     
